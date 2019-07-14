b0ff672d92ca5492f8a75d395264c32b511a2070
Mahlatse Mike Malete <mahlatse@Mahlatses-MacBook-Pro.local>
2019-07-14 16:26:18 +0200

    imports

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+  
+
 class TwitterBot:
     def __init__(self, email, password):
         self.username = username
