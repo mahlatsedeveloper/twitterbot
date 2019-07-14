5b9c6be455aade178b1009aa7bf193601bd0ee15
Mahlatse Mike Malete <mahlatse@Mahlatses-MacBook-Pro.local>
2019-07-14 16:18:23 +0200

    init

---
+++ app.py
@@ -0,0 +1,29 @@
+class TwitterBot:
+    def __init__(self, email, password):
+        self.username = username
+        self.password = password
+    
+
+    def login(self):
+        pass
+    
+
+    def follow(self):
+        pass
+    
+
+    def like(self):
+        pass
+    
+
+    def retweet(self):
+        pass
+    
+    
+    def tweet(self):
+        pass
+    
+
+bot = TwitterBot('email', 'password')
+bot.login()
+bot.follow()
